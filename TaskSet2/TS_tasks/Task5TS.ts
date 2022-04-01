8e180ce1b5b61990d4adbf795b8c68493b247b16
rucar12 <vasya19956a@gmail.com>
2022-04-02 00:59:34 +0300

    [#2]Add 5 TS tasks

---
+++ TaskSet2/TS_tasks/Task5TS.ts
@@ -0,0 +1,82 @@
+
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    {
+        type: 'admin',
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        type: 'user',
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        type: 'admin',
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    },
+    {
+        type: 'user',
+        name: 'Wilson',
+        age: 23,
+        occupation: 'Ball'
+    },
+    {
+        type: 'admin',
+        name: 'Agent Smith',
+        age: 23,
+        role: 'Administrator'
+    }
+];
+
+export const isAdmin = (person: Person): person is Admin => person.type === 'admin';
+export const isUser = (person: Person): person is User => person.type === 'user';
+
+export function logPerson(person: Person) {
+    let additionalInformation = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+export function filterUsers(persons: Person[], criteria: Partial<Person>): User[] {
+    return persons.filter(isUser).filter((user) => {
+        const criteriaKeys = Object.keys(criteria) as (keyof User)[];
+        return criteriaKeys.every((fieldName) => {
+            return {...persons, ...criteria}
+        });
+    });
+}
+
+console.log('Users of age 23:');
+
+filterUsers(
+    persons,
+    {
+        age: 23
+    }
+).forEach(logPerson);
